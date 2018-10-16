360761385d911dc43f6ddf1cf2f919652cea8993
chloe alcorn <ca.alcorn@outlook.com>
2018-10-16 16:10:34 +0100

    dodgems can get name from abstract method

--- src/main/java/ThemePark/Attraction/Dodgems.java
+++ src/main/java/ThemePark/Attraction/Dodgems.java
@@ -1,4 +1,9 @@
 package ThemePark.Attraction;
 
-public class Dodgems {
+public class Dodgems extends Attraction {
+
+    public Dodgems(String name){
+        super(name);
+
+    }
 }
--- src/test/java/DodgemsTest.java
+++ src/test/java/DodgemsTest.java
@@ -1,2 +1,20 @@
+import ThemePark.Attraction.Dodgems;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DodgemsTest {
+
+    Dodgems dodgems;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Bumper Cars");
+    }
+
+    @Test
+    public void getDodgemsName(){
+        assertEquals("Bumper Cars", dodgems.getName());
+    }
 }
