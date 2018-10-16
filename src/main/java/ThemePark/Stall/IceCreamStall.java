15720e64b7b08ae7ee03abc92613f1ae59348fca
chloe alcorn <ca.alcorn@outlook.com>
2018-10-16 17:09:11 +0100

    icecream stall added and tested

--- src/main/java/ThemePark/Stall/IceCreamStall.java
+++ src/main/java/ThemePark/Stall/IceCreamStall.java
@@ -1,4 +1,8 @@
 package ThemePark.Stall;
 
-public class IceCreamStall {
+public class IceCreamStall extends Stall {
+
+    public IceCreamStall(String name, String ownerName , int parkingSpaces){
+        super(name, ownerName, parkingSpaces);
+    }
 }
--- src/test/java/Stall/IceCreamStallTest.java
+++ src/test/java/Stall/IceCreamStallTest.java
@@ -1,4 +1,31 @@
 package Stall;
 
+import ThemePark.Stall.IceCreamStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IceCreamStallTest {
+    IceCreamStall iceCreamStall;
+
+    @Before
+    public void before(){
+        iceCreamStall = new IceCreamStall("Irenes Ices", "Carly Cream", 5);
+    }
+
+    @Test
+    public void iceCreamStallHasName(){
+        assertEquals("Irenes Ices", iceCreamStall.getName());
+    }
+
+    @Test
+    public void iceCreamStallHasOwner(){
+        assertEquals("Carly Cream", iceCreamStall.getOwnerName());
+    }
+
+    @Test
+    public void iceCreamStallHasParkingSpaces(){
+        assertEquals(5, iceCreamStall.getParkingSpaces());
+    }
 }
