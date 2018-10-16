d77f9a01c55366642322e1f7bc1611b89c18ef36
chloe alcorn <ca.alcorn@outlook.com>
2018-10-16 17:04:51 +0100

    tobacco stall added

--- src/main/java/ThemePark/Stall/TobaccoStall.java
+++ src/main/java/ThemePark/Stall/TobaccoStall.java
@@ -1,4 +1,8 @@
 package ThemePark.Stall;
 
-public class TobaccoStall {
+public class TobaccoStall extends Stall {
+
+    public TobaccoStall(String name, String ownerName, int parkingSpaces){
+        super(name, ownerName, parkingSpaces);
+    }
 }
--- src/test/java/Stall/TobaccoStallTest.java
+++ src/test/java/Stall/TobaccoStallTest.java
@@ -1,4 +1,31 @@
 package Stall;
 
+import ThemePark.Stall.TobaccoStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TobaccoStallTest {
+    TobaccoStall tobaccoStall;
+
+    @Before
+    public void before(){
+        tobaccoStall = new TobaccoStall("Tobys Tobacco", "Smokey Toby", 2);
+    }
+
+    @Test
+    public void tobaccoStallHasName(){
+        assertEquals("Tobys Tobacco", tobaccoStall.getName());
+    }
+
+    @Test
+    public void tobaccoStallHasOwner(){
+        assertEquals("Smokey Joe", tobaccoStall.getOwnerName());
+    }
+
+    @Test
+    public void tobaccoStallHasParkingSpaces(){
+        assertEquals(2, tobaccoStall.getParkingSpaces());
+    }
 }
